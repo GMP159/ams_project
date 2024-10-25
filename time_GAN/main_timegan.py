2131ac453b05939fc8e8940d388b6bef10c0165b
archana-ovgu <archana.yadav@st.ovgu.de>
2024-10-26 00:12:05 +0200

    added wandb to timeGAN

--- time_GAN/main_timegan.py
+++ time_GAN/main_timegan.py
@@ -29,6 +29,7 @@ from __future__ import print_function
 import argparse
 import numpy as np
 import warnings
+import os
 warnings.filterwarnings("ignore")
 
 # 1. TimeGAN model
@@ -36,9 +37,14 @@ from timegan import timegan
 # 2. Data loading
 from data_loading import real_data_loading, sine_data_generation
 # 3. Metrics
-# from metrics.discriminative_metrics import discriminative_score_metrics
-# from metrics.predictive_metrics import predictive_score_metrics
-# from metrics.visualization_metrics import visualization
+from metrics.discriminative_metrics import discriminative_score_metrics
+from metrics.predictive_metrics import predictive_score_metrics
+from metrics.visualization_metrics import visualization
+# 4. weights and biases
+import wandb
+
+wandb.login(key="32b931768d01c82d58c71f449d35c6aea7214264")
+
 
 
 def main (args):
@@ -61,10 +67,14 @@ def main (args):
     - metric_results: discriminative and predictive scores
   """
   ## Data loading
-  data = args.data
-  ori_data = real_data_loading(data, 24)
+  if args.data_name in ['stock', 'energy', 'CNC']:
+    ori_data = real_data_loading(args.data_name, args.seq_len)
+  elif args.data_name == 'sine':
+    # Set number of samples and its dimensions
+    no, dim = 10000, 5
+    ori_data = sine_data_generation(no, args.seq_len, dim)
     
-  print(' Dataset is loaded and ready.')
+  print(args.data_name + ' dataset is ready.')
     
   ## Synthetic data generation by TimeGAN
   # Set newtork parameters
@@ -74,39 +84,61 @@ def main (args):
   parameters['num_layer'] = args.num_layer
   parameters['iterations'] = args.iteration
   parameters['batch_size'] = args.batch_size
-  parameters['max_time'] = args.max_time
-      
-  generated_data = timegan(ori_data, parameters)   
-  print('Finish Synthetic Data Generation')
-  
-  # ## Performance metrics   
-  # # Output initialization
-  # metric_results = dict()
-  
-  # # 1. Discriminative Score
-  # discriminative_score = list()
-  # for _ in range(args.metric_iteration):
-  #   temp_disc = discriminative_score_metrics(ori_data, generated_data)
-  #   discriminative_score.append(temp_disc)
-      
-  # metric_results['discriminative'] = np.mean(discriminative_score)
-      
-  # # 2. Predictive score
-  # predictive_score = list()
-  # for tt in range(args.metric_iteration):
-  #   temp_pred = predictive_score_metrics(ori_data, generated_data)
-  #   predictive_score.append(temp_pred)   
-      
-  # metric_results['predictive'] = np.mean(predictive_score)     
-          
-  # # 3. Visualization (PCA and tSNE)
-  # visualization(ori_data, generated_data, 'pca')
-  # visualization(ori_data, generated_data, 'tsne')
-  
-  # ## Print discriminative and predictive scores
-  # print(metric_results)
 
-  # return ori_data, generated_data, metric_results
+  # wandb initialisation
+  wandb.init(project='Transformer_GAN', config={
+      parameters['module'] = args.module
+  parameters['hidden_dim'] = args.hidden_dim
+  parameters['num_layer'] = args.num_layer
+  parameters['iterations'] = args.iteration
+  parameters['batch_size'] = args.batch_size
+  })
+        
+  
+  generated_data = timegan(ori_data, parameters)
+  #flatten the 3D generated data to 2D data 
+  flattened_data = generated_data.reshape(-1, generated_data.shape[-1])
+  # Save to CSV
+  np.savetxt('output.csv', flattened_data, delimiter=',') 
+     
+  print('Finish Synthetic Data Generation')
+
+  
+  ## Performance metrics   
+  # Output initialization
+  metric_results = dict()
+  
+  # 1. Discriminative Score
+  discriminative_score = list()
+  for _ in range(args.metric_iteration):
+    temp_disc = discriminative_score_metrics(ori_data, generated_data)
+    discriminative_score.append(temp_disc)
+      
+  metric_results['discriminative'] = np.mean(discriminative_score)
+  #log discriminative score on wandb
+  wandb.log({"Discriminative Score": np.mean(discriminative_score)})
+      
+  # 2. Predictive score
+  predictive_score = list()
+  for tt in range(args.metric_iteration):
+    temp_pred = predictive_score_metrics(ori_data, generated_data)
+    predictive_score.append(temp_pred)   
+      
+  metric_results['predictive'] = np.mean(predictive_score)
+
+  #log predictive score on wandb
+   wandb.log({"Predictive Score": np.mean(predictive_score)}) 
+
+  ## Print discriminative and predictive scores
+  print(metric_results) 
+          
+  # 3. Visualization (PCA and tSNE)
+  visualization(ori_data, generated_data, 'pca')
+  visualization(ori_data, generated_data, 'tsne')
+ 
+  
+
+  return ori_data, generated_data, metric_results
 
 
 if __name__ == '__main__':  
@@ -114,8 +146,9 @@ if __name__ == '__main__':
   # Inputs for the main function
   parser = argparse.ArgumentParser()
   parser.add_argument(
-      '--data',
-      help='data file name',
+      '--data_name',
+      choices=['sine','stock','energy','CNC'],
+      default='stock',
       type=str)
   parser.add_argument(
       '--seq_len',
@@ -124,13 +157,13 @@ if __name__ == '__main__':
       type=int)
   parser.add_argument(
       '--module',
-      choices=['gru','lstm','rnn'],
+      choices=['gru','lstm','lstmLN'],
       default='gru',
       type=str)
   parser.add_argument(
       '--hidden_dim',
       help='hidden state dimensions (should be optimized)',
-      default=22,
+      default=24,
       type=int)
   parser.add_argument(
       '--num_layer',
@@ -148,15 +181,10 @@ if __name__ == '__main__':
       default=128,
       type=int)
   parser.add_argument(
-      '--max_time',
-      help='the number of max time stamps (should be optimized)',
-      default=22,
+      '--metric_iteration',
+      help='iterations of the metric computation',
+      default=10,
       type=int)
-  # parser.add_argument(
-  #     '--metric_iteration',
-  #     help='iterations of the metric computation',
-  #     default=10,
-  #     type=int)
   
   args = parser.parse_args() 
   
--- time_GAN/metrics/predictive_metrics.py
+++ time_GAN/metrics/predictive_metrics.py
@@ -11,113 +11,111 @@ Code author: Jinsung Yoon (jsyoon0823@gmail.com)
 
 -----------------------------
 
-predictive_metrics.py
+visualization_metrics.py
 
-Note: Use Post-hoc RNN to predict one-step ahead (last feature)
+Note: Use PCA or tSNE for generated and original data visualization
 """
 
-# Necessary Packages
-import tensorflow as tf
+# Necessary packages
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.metrics import mean_absolute_error
-from utils import extract_time
+import os
 
- 
-def predictive_score_metrics (ori_data, generated_data):
-  """Report the performance of Post-hoc RNN one-step ahead prediction.
+   
+def visualization (ori_data, generated_data, analysis):
+  """Using PCA or tSNE for generated and original data visualization.
   
   Args:
     - ori_data: original data
     - generated_data: generated synthetic data
+    - analysis: tsne or pca
+  """  
+  # Analysis sample size (for faster computation)
+  anal_sample_no = min([1000, len(ori_data)])
+  idx = np.random.permutation(len(ori_data))[:anal_sample_no]
     
-  Returns:
-    - predictive_score: MAE of the predictions on the original data
-  """
-  # Initialization on the Graph
-  tf.reset_default_graph()
+  # Data preprocessing
+  ori_data = np.asarray(ori_data)
+  generated_data = np.asarray(generated_data)  
+  
+  ori_data = ori_data[idx]
+  generated_data = generated_data[idx]
+  
+  no, seq_len, dim = ori_data.shape  
+  
+  for i in range(anal_sample_no):
+    if (i == 0):
+      prep_data = np.reshape(np.mean(ori_data[0,:,:], 1), [1,seq_len])
+      prep_data_hat = np.reshape(np.mean(generated_data[0,:,:],1), [1,seq_len])
+    else:
+      prep_data = np.concatenate((prep_data, 
+                                  np.reshape(np.mean(ori_data[i,:,:],1), [1,seq_len])))
+      prep_data_hat = np.concatenate((prep_data_hat, 
+                                      np.reshape(np.mean(generated_data[i,:,:],1), [1,seq_len])))
+    
+  # Visualization parameter        
+  colors = ["red" for i in range(anal_sample_no)] + ["blue" for i in range(anal_sample_no)]    
+    
+  if analysis == 'pca':
+    # PCA Analysis
+    pca = PCA(n_components = 2)
+    pca.fit(prep_data)
+    pca_results = pca.transform(prep_data)
+    pca_hat_results = pca.transform(prep_data_hat)
+    
+    # Plotting
+    #f, ax = plt.subplots(1)
+    plt.figure()    
+    plt.scatter(pca_results[:,0], pca_results[:,1],
+                c = colors[:anal_sample_no], alpha = 0.2, label = "Original")
+    plt.scatter(pca_hat_results[:,0], pca_hat_results[:,1], 
+                c = colors[anal_sample_no:], alpha = 0.2, label = "Synthetic")
+  
+    ax= plt.gca()
+    ax.legend()  
+    plt.title('PCA plot')
+    plt.xlabel('x-pca')
+    plt.ylabel('y_pca')
+    plt.savefig('pca_plot.png', format='png')
+    plt.show()
 
-  # Basic Parameters
-  no, seq_len, dim = np.asarray(ori_data).shape
+    # Save the plot to in the results folder
+    results_folder = '/content/drive/MyDrive/TimeGAN_cnc/results'
+    os.makedirs(results_folder, exist_ok=True)
+    save_path = os.path.join(results_folder, 'pca_plot.png')
+    plt.savefig(save_path, format='png')
     
-  # Set maximum sequence length and each sequence length
-  ori_time, ori_max_seq_len = extract_time(ori_data)
-  generated_time, generated_max_seq_len = extract_time(ori_data)
-  max_seq_len = max([ori_max_seq_len, generated_max_seq_len])  
-     
-  ## Builde a post-hoc RNN predictive network 
-  # Network parameters
-  hidden_dim = int(dim/2)
-  iterations = 5000
-  batch_size = 128
+  elif analysis == 'tsne':
     
-  # Input place holders
-  X = tf.placeholder(tf.float32, [None, max_seq_len-1, dim-1], name = "myinput_x")
-  T = tf.placeholder(tf.int32, [None], name = "myinput_t")    
-  Y = tf.placeholder(tf.float32, [None, max_seq_len-1, 1], name = "myinput_y")
+    # Do t-SNE Analysis together       
+    prep_data_final = np.concatenate((prep_data, prep_data_hat), axis = 0)
     
-  # Predictor function
-  def predictor (x, t):
-    """Simple predictor function.
-    
-    Args:
-      - x: time-series data
-      - t: time information
+    # TSNE anlaysis
+    tsne = TSNE(n_components = 2, verbose = 1, perplexity = 40, n_iter = 300)
+    tsne_results = tsne.fit_transform(prep_data_final)
       
-    Returns:
-      - y_hat: prediction
-      - p_vars: predictor variables
-    """
-    with tf.variable_scope("predictor", reuse = tf.AUTO_REUSE) as vs:
-      p_cell = tf.nn.rnn_cell.GRUCell(num_units=hidden_dim, activation=tf.nn.tanh, name = 'p_cell')
-      p_outputs, p_last_states = tf.nn.dynamic_rnn(p_cell, x, dtype=tf.float32, sequence_length = t)
-      y_hat_logit = tf.contrib.layers.fully_connected(p_outputs, 1, activation_fn=None) 
-      y_hat = tf.nn.sigmoid(y_hat_logit)
-      p_vars = [v for v in tf.all_variables() if v.name.startswith(vs.name)]
-    
-    return y_hat, p_vars
-    
-  y_pred, p_vars = predictor(X, T)
-  # Loss for the predictor
-  p_loss = tf.losses.absolute_difference(Y, y_pred)
-  # optimizer
-  p_solver = tf.train.AdamOptimizer().minimize(p_loss, var_list = p_vars)
-        
-  ## Training    
-  # Session start
-  sess = tf.Session()
-  sess.run(tf.global_variables_initializer())
-    
-  # Training using Synthetic dataset
-  for itt in range(iterations):
-          
-    # Set mini-batch
-    idx = np.random.permutation(len(generated_data))
-    train_idx = idx[:batch_size]     
-            
-    X_mb = list(generated_data[i][:-1,:(dim-1)] for i in train_idx)
-    T_mb = list(generated_time[i]-1 for i in train_idx)
-    Y_mb = list(np.reshape(generated_data[i][1:,(dim-1)],[len(generated_data[i][1:,(dim-1)]),1]) for i in train_idx)        
-          
-    # Train predictor
-    _, step_p_loss = sess.run([p_solver, p_loss], feed_dict={X: X_mb, T: T_mb, Y: Y_mb})        
-    
-  ## Test the trained model on the original data
-  idx = np.random.permutation(len(ori_data))
-  train_idx = idx[:no]
-    
-  X_mb = list(ori_data[i][:-1,:(dim-1)] for i in train_idx)
-  T_mb = list(ori_time[i]-1 for i in train_idx)
-  Y_mb = list(np.reshape(ori_data[i][1:,(dim-1)], [len(ori_data[i][1:,(dim-1)]),1]) for i in train_idx)
-    
-  # Prediction
-  pred_Y_curr = sess.run(y_pred, feed_dict={X: X_mb, T: T_mb})
-    
-  # Compute the performance in terms of MAE
-  MAE_temp = 0
-  for i in range(no):
-    MAE_temp = MAE_temp + mean_absolute_error(Y_mb[i], pred_Y_curr[i,:,:])
-    
-  predictive_score = MAE_temp / no
-    
-  return predictive_score
-    
+    # Plotting
+    #f, ax = plt.subplots(1)
+    plt.figure()   
+    plt.scatter(tsne_results[:anal_sample_no,0], tsne_results[:anal_sample_no,1], 
+                c = colors[:anal_sample_no], alpha = 0.2, label = "Original")
+    plt.scatter(tsne_results[anal_sample_no:,0], tsne_results[anal_sample_no:,1], 
+                c = colors[anal_sample_no:], alpha = 0.2, label = "Synthetic")
+  
+
+    ax= plt.gca()
+    ax.legend()
+      
+    plt.title('t-SNE plot')
+    plt.xlabel('x-tsne')
+    plt.ylabel('y_tsne')
+    plt.savefig('tsne_plot.png', format='png')
+    plt.show()  
+
+    # Save the plot to in the results folder
+    results_folder = '/content/drive/MyDrive/TimeGAN_cnc/results'
+    os.makedirs(results_folder, exist_ok=True)
+    save_path = os.path.join(results_folder, 'tsne_plot.png')
+    plt.savefig(save_path, format='png')  
